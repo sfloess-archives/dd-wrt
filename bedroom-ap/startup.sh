4453b2b4fc7adbf3eca6f6af7b0348fd6e99bc7c
Scot P. Floess <sfloess@nc.rr.com>
2019-07-16 10:26:47 -0400

    NA - update the nsswitch.conf file as well as starting various cloud backup mounts

--- bedroom-ap/startup.sh
+++ bedroom-ap/startup.sh
@@ -21,4 +21,12 @@ chmod 700 -R /etc/ssh /var/run/sshd
 /etc/init.d/nis        start
 /etc/init.d/ssh        start
 
+/usr/bin/rclone mount --allow-other --daemon Box:        /mnt/cloud/Box
+/usr/bin/rclone mount --allow-other --daemon Dropbox:    /mnt/cloud/Dropbox
+/usr/bin/rclone mount --allow-other --daemon Google:     /mnt/cloud/Google
+/usr/bin/rclone mount --allow-other --daemon Jottacloud: /mnt/cloud/Jottacloud
+/usr/bin/rclone mount --allow-other --daemon Microsoft:  /mnt/cloud/Microsoft
+/usr/bin/rclone mount --allow-other --daemon Yandex:     /mnt/cloud/Yandex
+/usr/bin/rclone mount --allow-other --daemon pCloud:     /mnt/cloud/pCloud
+
 nohup /usr/local/bin/mega-cmd-server &
