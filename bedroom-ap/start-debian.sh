cdd013fc700c895c2c0a6411df3ee8b7ee44a9cf
Scot P. Floess <sfloess@nc.rr.com>
2019-07-17 23:36:12 -0400

    NA - finalizing mounts

--- bedroom-ap/start-debian.sh
+++ bedroom-ap/start-debian.sh
@@ -21,14 +21,24 @@ chmod 700 -R /etc/ssh /var/run/sshd
 /etc/init.d/nis        start
 /etc/init.d/ssh        start
 
-/root/gopath/bin/rclone mount --allow-other --daemon Box:/FlossWare        /mnt/cloud/Box
-/root/gopath/bin/rclone mount --allow-other --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
-/root/gopath/bin/rclone mount --allow-other --daemon Google:/FlossWare     /mnt/cloud/Google
-/root/gopath/bin/rclone mount --allow-other --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
-/root/gopath/bin/rclone mount --allow-other --daemon Mega:/FlossWare       /mnt/cloud/Mega
-/root/gopath/bin/rclone mount --allow-other --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
-/root/gopath/bin/rclone mount --allow-other --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
-/root/gopath/bin/rclone mount --allow-other --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
-/root/gopath/bin/rclone mount --allow-other --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Box:/FlossWare        /mnt/cloud/Box
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Google:/FlossWare     /mnt/cloud/Google
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Mega:/FlossWare       /mnt/cloud/Mega
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
+#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
+
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Box:/FlossWare        /mnt/cloud/Box
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Google:/FlossWare     /mnt/cloud/Google
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Mega:/FlossWare       /mnt/cloud/Mega
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
+/usr/bin/sudo /root/gopath/bin/rclone mount --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
 
 nohup /usr/local/bin/mega-cmd-server &
--- desktop-ap/start-entware.sh
+++ desktop-ap/start-entware.sh
@@ -2,9 +2,9 @@
 
 # ---------------------------------------------------------------------
 
-mkdir -p /mnt/bedroom-ap
+mkdir -p /tmp/mnt/sda1/debian_armel/mnt/bedroom-ap/cloud
 
-/opt/bin/nfusr nfs://bedroom-ap/tmp/mnt/sda1/debian_armel/mnt/cloud /mnt/bedroom-ap
+/opt/bin/nfusr nfs://bedroom-ap/tmp/mnt/sda1/debian_armel/mnt/cloud /tmp/mnt/sda1/debian_armel/mnt/bedroom-ap/cloud
 
 # ---------------------------------------------------------------------
 
