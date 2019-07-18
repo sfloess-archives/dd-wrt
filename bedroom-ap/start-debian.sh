63d5a591c58da55a43ec7d776c55d45941507d20
Scot P. Floess <sfloess@nc.rr.com>
2019-07-17 23:57:16 -0400

    NA - set execute bit on stop-debian.sh and adjusted mount options on start-debian.sh

--- bedroom-ap/start-debian.sh
+++ bedroom-ap/start-debian.sh
@@ -21,24 +21,24 @@ chmod 700 -R /etc/ssh /var/run/sshd
 /etc/init.d/nis        start
 /etc/init.d/ssh        start
 
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Box:/FlossWare        /mnt/cloud/Box
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Google:/FlossWare     /mnt/cloud/Google
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Mega:/FlossWare       /mnt/cloud/Mega
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
-#/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
+mkdir -p /mnt/cloud/Box
+mkdir -p /mnt/cloud/Dropbox
+mkdir -p /mnt/cloud/Google
+mkdir -p /mnt/cloud/Jottacloud
+mkdir -p /mnt/cloud/Mega
+mkdir -p /mnt/cloud/Microsoft
+mkdir -p /mnt/cloud/OpenDrive
+mkdir -p /mnt/cloud/Yandex
+mkdir -p /mnt/cloud/pCloud
 
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Box:/FlossWare        /mnt/cloud/Box
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Google:/FlossWare     /mnt/cloud/Google
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Mega:/FlossWare       /mnt/cloud/Mega
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
-/usr/bin/sudo /root/gopath/bin/rclone mount --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Box:/FlossWare        /mnt/cloud/Box
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Google:/FlossWare     /mnt/cloud/Google
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Mega:/FlossWare       /mnt/cloud/Mega
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
+/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
 
 nohup /usr/local/bin/mega-cmd-server &
