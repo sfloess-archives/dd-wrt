62592fcbf195c5c914f191c432cf6d9b7dc7cd82
Scot P. Floess <sfloess@nc.rr.com>
2019-07-18 17:15:10 -0400

    NA - added in mhddfs for combining cloud drives into one big storage space

--- bedroom-ap/start-debian.sh
+++ bedroom-ap/start-debian.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# ----------------------------------------------------
+
 # Needed for Mega cloud backups
 export LD_LIBRARY_PATH=/usr/local/lib/
+
+# ----------------------------------------------------
 
 chmod -R 700 /root/.ssh
 chmod 777 /run/screen
@@ -9,9 +13,13 @@ chmod 777 /run/screen
 chmod u+s /usr/bin/sudo
 chmod g+s /usr/bin/sudo
 
+# ----------------------------------------------------
+
 /usr/sbin/ntpdate-debian
 
 chmod 700 -R /etc/ssh /var/run/sshd
+
+# ----------------------------------------------------
 
 /etc/init.d/rsyslog    start
 /etc/init.d/ntp        start
@@ -59,4 +67,16 @@ chmod 755 /mnt/cloud/*
 #/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --vfs-cache-mode full --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
 #/usr/bin/sudo /root/gopath/bin/rclone mount --allow-other --vfs-cache-mode full --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
 
+# ----------------------------------------------------
+# Combine individual drives into one...
+
+mkdir -p /mnt/cloud/Shared 
+
+mhddfs /mnt/cloud/Box /mnt/cloud/OpenDrive /mnt/cloud/Shared
+
+# ----------------------------------------------------
+
 nohup /usr/local/bin/mega-cmd-server &
+
+# ----------------------------------------------------
+
