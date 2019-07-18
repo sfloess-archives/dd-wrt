1840180d76fa262364a58b43384c92ec2e04c3bb
Scot P. Floess <sfloess@nc.rr.com>
2019-07-17 22:45:49 -0400

    NA - all cloud dirs are now structured as [rclone name]:/FlossWare

--- bedroom-ap/start-debian.sh
+++ bedroom-ap/start-debian.sh
@@ -21,13 +21,14 @@ chmod 700 -R /etc/ssh /var/run/sshd
 /etc/init.d/nis        start
 /etc/init.d/ssh        start
 
-/usr/bin/rclone mount --allow-other --daemon Box:        /mnt/cloud/Box
-/usr/bin/rclone mount --allow-other --daemon Dropbox:    /mnt/cloud/Dropbox
-/usr/bin/rclone mount --allow-other --daemon Google:     /mnt/cloud/Google
-/usr/bin/rclone mount --allow-other --daemon Jottacloud: /mnt/cloud/Jottacloud
-/usr/bin/rclone mount --allow-other --daemon Microsoft:  /mnt/cloud/Microsoft
-/usr/bin/rclone mount --allow-other --daemon OpenDrive:  /mnt/cloud/OpenDrive
-/usr/bin/rclone mount --allow-other --daemon Yandex:     /mnt/cloud/Yandex
-/usr/bin/rclone mount --allow-other --daemon pCloud:     /mnt/cloud/pCloud
+/root/gopath/bin/rclone mount --allow-other --daemon Box:/FlossWare        /mnt/cloud/Box
+/root/gopath/bin/rclone mount --allow-other --daemon Dropbox:/FlossWare    /mnt/cloud/Dropbox
+/root/gopath/bin/rclone mount --allow-other --daemon Google:/FlossWare     /mnt/cloud/Google
+/root/gopath/bin/rclone mount --allow-other --daemon Jottacloud:/FlossWare /mnt/cloud/Jottacloud
+/root/gopath/bin/rclone mount --allow-other --daemon Mega:/FlossWare       /mnt/cloud/Mega
+/root/gopath/bin/rclone mount --allow-other --daemon Microsoft:/FlossWare  /mnt/cloud/Microsoft
+/root/gopath/bin/rclone mount --allow-other --daemon OpenDrive:/FlossWare  /mnt/cloud/OpenDrive
+/root/gopath/bin/rclone mount --allow-other --daemon Yandex:/FlossWare     /mnt/cloud/Yandex
+/root/gopath/bin/rclone mount --allow-other --daemon pCloud:/FlossWare     /mnt/cloud/pCloud
 
 nohup /usr/local/bin/mega-cmd-server &
